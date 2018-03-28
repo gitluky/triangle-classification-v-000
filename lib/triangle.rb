228566863460dd77412d1f0d1a058625ecbcabde
Jonathan Luk <jluk87@gmail.com>
2018-03-28 19:26:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,18 +2,30 @@ class Triangle
   attr_accessor :type
 
   def initialize(a,b,c)
-
-
+    sides = [a,b,c]
+    kind(sides)
   end
 
-  def kind
+  def kind(sides)
+    if sides.any? {|side| side <= 0} || sides[0] + sides[1] < sides[2]
+      begin
+        raise TriangleError
+      rescue
+        
+      end
+
+
+
+    
     @type
 
   end
 
 
   class TriangleError < StandardError
-
+    def message
+      puts "Invalid Triangle"
+    end
   end
 
 end
