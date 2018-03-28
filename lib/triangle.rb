0ca45f9ca72de784ec3c77621b689cec61dd1267
Jonathan Luk <jluk87@gmail.com>
2018-03-28 18:27:07 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,6 +15,18 @@ class Triangle
     end
   end
 
+    def identify(a,b,c)
+      case type
+        when a == b && b == c
+          self.kind = :equilateral
+        when a == b || b == c || a == c
+          self.kind == :isosceles
+        else
+          self.kind == :scalene
+      end
+
+    end
+
   class TriangleError < StandardError
 
   end
