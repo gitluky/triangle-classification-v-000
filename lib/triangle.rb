12298ef0773b411161aecd5f21fb79f9b62d1e32
Jonathan Luk <jluk87@gmail.com>
2018-03-28 18:52:52 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,19 @@ class Triangle
     def identify(a,b,c)
       sides = [a,b,c]
       if sides.all?{|side| side > 0} && sides[0] + sides[1] > sides[2]
-
+        if a == b && b == c
+          self.kind = :equilateral
+        elsif a == b || b == c || a == c
+          self.kind = :isosceles
+        else
+          self.kind = :scalene
+        end
+      else
+        begin
+          raise TriangleError
+        rescue TriangleError => error
+          error.message
+        end
     end
 
   class TriangleError < StandardError
