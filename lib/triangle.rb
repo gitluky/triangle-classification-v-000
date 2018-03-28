ce257997451228668e169fd8afc1b414eff33c86
Jonathan Luk <jluk87@gmail.com>
2018-03-28 18:15:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  attr_accessor :kind
+
+  @@kind = [:quilateral, :isosceles, :scalene]
+
+  def initialize(kind)
+    if self.class.kind.none?(kind)
+      begin
+        raise KindException
+      rescue KindException => error
+        error.message
+      end
+    else
+      @kind = kind
+    end
+  end
+  
+      
+
+    
+
+  
 end
