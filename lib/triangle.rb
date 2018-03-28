3f46099f4f48f2b5f5a5b70415c3152e5450fcb7
Jonathan Luk <jluk87@gmail.com>
2018-03-28 18:27:33 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,15 +4,7 @@ class Triangle
   @@kind = [:quilateral, :isosceles, :scalene]
 
   def initialize(length_1,length_2,length_3)
-    if self.class.kind.none?(kind)
-      begin
-        raise TriangleError
-      rescue TriangleError => error
 
-      end
-    else
-      @kind = kind
-    end
   end
 
     def identify(a,b,c)
