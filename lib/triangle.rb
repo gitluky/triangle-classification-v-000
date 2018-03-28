3655a5b10341eaca9694a2296e066895f8d593ed
Jonathan Luk <jluk87@gmail.com>
2018-03-28 19:19:43 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,34 +1,18 @@
 class Triangle
   attr_accessor :kind
 
-  def initialize(side_a, side_b, side_c)
-    self.kind(side_a, side_b, side_c)
+  def initialize(a,b,c)
+
+
   end
 
-  def kind(a,b,c)
-    side_array = [a,b,c]
-    if side_array.any?{|side| side <= 0} || side_array[0] + side_array[1] < side_array[2]
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-        error.message
-      end
-    else
-      if side_array[0] == side_array[1] && side_array[1] == side_array[2]
-        @kind = :equilateral
-      elsif side_array[0] == side_array[1] && side_array[0] != side_array[2]
-        @kind = :isosceles
-      else
-        @kind = :scalene
-      end
-    end
+  def kind
+
   end
+
 
   class TriangleError < StandardError
-    def message
-      puts "Invalid Triangle"
-    end
-  end
 
+  end
 
 end
